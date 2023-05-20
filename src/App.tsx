d452ca360eab9851d464726787e273aa75ce2fe5
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-20 23:50:19 +0800

    change background asset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import "./styles/Main.css";
 
 function App() {
   const apiKey = `6f8effcef36b38afb38fc70bbfd51996`;
@@ -8,14 +9,14 @@ function App() {
 
   return (
     <>
-      <div className="p-0 m-0 box-border font-poppins">
+      <div className="Main p-0 m-0 box-border font-poppins">
         <div className="flex flex-col items-center space-y-60">
           <div className="mt-40">
             <h1 className="text-4xl">Dallas</h1>
             <h1 className="text-6xl font-bold">60</h1>
             <p className="text-2xl italic text-gray-700">Rainy</p>
           </div>
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between items-center border-2 border-black p-12 rounded bg-slate-100">
             <div className="mx-5 text-center">
               <p className="text-3xl font-bold">65</p>
               <p className="text-2xl">Feels Like</p>
