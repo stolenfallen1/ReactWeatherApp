e77d6268dcc27a52db92b644db2bc108635e4687
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-20 22:02:31 +0800

    api keys, basic structure and asset img

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const apiKey = `6f8effcef36b38afb38fc70bbfd51996`;
+
+  //https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
 
   return (
     <>
       <div className="p-0 m-0 box-border">
-        <h1 className="text-2xl font-bold tracking-tighter text-center">
-          Hello World
-        </h1>
+        <div>
+          <div>
+            <h1>Dallas</h1>
+          </div>
+          <div>
+            <h1>60</h1>
+          </div>
+          <div>
+            <p>Rainy</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>65</p>
+          </div>
+          <div>
+            <p>20%</p>
+          </div>
+          <div>
+            <p>12 MPH</p>
+          </div>
+        </div>
       </div>
     </>
   );
